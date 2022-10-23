20e95a4c9188804c674de8d33aa11143f661d605
Bekzodjon Norkuziev <bekzodnx@gmail.com>
2022-10-23 06:14:12 +0200

    CSS, building block, sections attrs

--- client/my-app/README.md
+++ client/my-app/README.md
@@ -1,8 +1,12 @@
-# Getting Started with Create React App
+# Unwrapped client app
+
+Used React library
+
+### Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-## Available Scripts
+### Available Scripts
 
 In the project directory, you can run:
 
--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [activeSectionNumber, setActiveSectionNumber] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <main className='main'>
+        <div className='header'>
+          <h2>
+            Discover an insight <span>🚀</span>{' '}
+          </h2>
+        </div>
+        <div className='sections_wrap'>
+          <h3>SECTIONS</h3>
+          <div className='button_container'>
+            <button
+              className={
+                'section_btn' +
+                (activeSectionNumber === 1 ? ' activeSectionButton' : '')
+              }
+              onClick={() => setActiveSectionNumber(1)}
+            >
+              Customers
+            </button>
+            <button
+              className={
+                'section_btn' +
+                (activeSectionNumber === 2 ? ' activeSectionButton' : '')
+              }
+              onClick={() => setActiveSectionNumber(2)}
+            >
+              Rewards
+            </button>
+            <button
+              className={
+                'section_btn' +
+                (activeSectionNumber === 3 ? ' activeSectionButton' : '')
+              }
+              onClick={() => setActiveSectionNumber(3)}
+            >
+              Activities
+            </button>
+          </div>
+        </div>
+        <div className='blocks_wrap'>
+          <h3>FUNCTIONS</h3>
+          <button className='block_btn'>
+            How many users who does login daily?
+          </button>
+          <button className='block_btn'>
+            Total number of users who levelled down the membership ?
+          </button>
+          <button className='block_btn'>
+            How many users are setted the Gold membership ?
+          </button>
+        </div>
+      </main>
     </div>
   );
 }
